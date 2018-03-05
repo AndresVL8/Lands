138ec1c80ab948c24a0e223ad637cfae75f9aa02
Andres Velez <avandrews669@gmail.com>
2018-03-04 21:26:18 -0500

    FINISH video 17

--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace Lands.ViewModels
+﻿
+
+namespace Lands.ViewModels
 {
+    using Models;
+    using System.Collections.Generic;
     public class MainViewModel
     {
+        #region Properties
+        public List<Land> LandsList
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
         #region ViewModels
         public LoginViewModel Login
         {
